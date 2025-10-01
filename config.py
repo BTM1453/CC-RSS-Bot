0216cd391961692694a8d91e294ca7b14e7c05ce
Muhammed Aflah <aflahtbm@gmail.com>
2025-10-01 10:39:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class BOT:
     """
     TOKEN: Bot token generated from @BotFather
     """
-    TOKEN = os.environ.get("TOKEN", "")
+    TOKEN = os.environ.get("TOKEN", "7733732635:AAG1GFhxe4qDUH8KH57J5DnntMlQwc4hVic")
 
 
 class API:
@@ -17,22 +17,22 @@ class API:
     HASH: Telegram API hash from https://my.telegram.org
     ID = Telegram API ID from https://my.telegram.org
     """
-    HASH = os.environ.get("API_HASH", "")
-    ID = int(os.environ.get("API_ID", 0))
+    HASH = os.environ.get("API_HASH", "c16b13b73c2f2473b8e3cbcf2ab1d200")
+    ID = int(os.environ.get("API_ID", 24638343))
 
 
 class OWNER:
     """
     ID: Owner's user id, get it from @userinfobot
     """
-    ID = int(os.environ.get("OWNER", 0))
+    ID = int(os.environ.get("OWNER", 6272165202))
 
 
 class CHANNEL:
     """
     ID: Telegram Channel ID where the bot will post automatically
     """
-    ID = int(os.environ.get("CHANNEL_ID", 0))
+    ID = int(os.environ.get("CHANNEL_ID", -1002983976336))
 
 
 class WEB:
